d7e6bc37787e4e32c5fa045282e784e82820289d
Juan Gabino <juan_gabino@usmp.pe>
2019-05-07 12:56:56 -0500

    Agregar archivos de proyecto.

---
+++ tallerprueba/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace tallerprueba.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
